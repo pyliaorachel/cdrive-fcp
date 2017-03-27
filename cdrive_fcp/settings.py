9e3bb5b0f42c2c2c656671bf0c8df6c32ca26f0d
Rachel P.Y. Liao <u3512485@connect.hku.hk>
2017-03-27 20:43:48 +0800

    use default authentication packages

--- cdrive_fcp/settings.py
+++ cdrive_fcp/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'cdrive_fcp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, 'static'),
+]
+
+# Others
+LOGIN_REDIRECT_URL = '/homepage'
